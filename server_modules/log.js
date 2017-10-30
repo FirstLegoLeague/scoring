03c6e8f58ee549aeec6a4e71209c2c119de96d2c
Idan Stark <idanstark42@gmail.com>
2017-10-30 13:15:48 +0200

    fixed setting headers after response is sent

--- server_modules/log.js
+++ server_modules/log.js
@@ -33,6 +33,6 @@ exports.beforeLayer = function(req, res, next) {
 };
 
 exports.afterLayer = function(req, res, next) {
-    req.log.debug(`Complete error status ${res.statusCode}`);
+    exports.log(`Completed status ${res.statusCode}`);
     next();
 };
