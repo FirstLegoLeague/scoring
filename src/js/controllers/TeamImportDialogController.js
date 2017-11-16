7d9469f8b42364a9639429b4f5439d0e6d40a4fc
ThinkRedstone <ittai11@gmail.com>
2017-11-16 15:16:45 +0200

    added the ability to drop csv file to import them

--- src/js/controllers/TeamImportDialogController.js
+++ src/js/controllers/TeamImportDialogController.js
@@ -1,20 +1,20 @@
 define('controllers/TeamImportDialogController', [
+    'ng-file-upload',
     'services/ng-handshake',
     'angular'
 ], function () {
     var moduleName = 'TeamImportDialog';
 
-    return angular.module(moduleName, []).controller('TeamImportDialogController', [
+    return angular.module(moduleName, ['ngFileUpload']).controller('TeamImportDialogController', [
         '$scope', '$handshake',
         function ($scope, $handshake) {
             var defer;
 
-            function parseData(data) {
+            function parseData(data, headerLength) {
                 //parse raw import, split lines
-                var lines = data ? data.split(/[\n\r]/) : [];
-                if ($scope.importHeader) {
-                    lines.shift();
-                }
+                var lines = data ? data.match(/[^\r\n]+/g) : [];
+                headerLength = headerLength ? headerLength : 0;
+                lines.splice(0, headerLength);
                 lines = lines.map(function (line) {
                     if ($scope.useCustomDelimiter) {
                         return line.split($scope.delimiter);
@@ -26,6 +26,8 @@ define('controllers/TeamImportDialogController', [
                 $scope.importNumberColumn = 1;
                 $scope.importNameColumn = 2;
 
+                lines = lines.filter((parsedLine) => parsedLine[$scope.importNumberColumn - 1] !== ""); //filter lines which don't contain a team- we do this by looking for a team number
+
                 if (lines[0]) {
                     $scope.importNumberExample = lines[0][$scope.importNumberColumn - 1];
                     $scope.importNameExample = lines[0][$scope.importNameColumn - 1];
@@ -33,24 +35,39 @@ define('controllers/TeamImportDialogController', [
                     $scope.importNumberExample = '';
                     $scope.importNameExample = '';
                 }
+                console.log(lines);
 
                 $scope.importLines = lines;
             }
 
+            $scope.upload = function (files, file) {
+                if (file === null) {
+                    //this means a non-csv file was uploaded, ignore
+                    return;
+                }
+                var reader = new FileReader();
+                $scope.useCustomDelimiter = true;
+                $scope.delimiter = ",";
+                $scope.headerLength = 3;
+                reader.onload = (event) => $scope.importRaw = event.target.result; //no need to explicitly call parseData(), because it will trigger on it's own when we change $scope.importRaw
+                reader.readAsText(file);
+
+            };
+
             $scope.$watch('importRaw', function (data) {
-                parseData($scope.importRaw);
+                parseData($scope.importRaw, $scope.headerLength);
             });
 
-            $scope.$watch('importHeader', function (data) {
-                parseData($scope.importRaw);
+            $scope.$watch('headerLength', function (data) {
+                parseData($scope.importRaw, $scope.headerLength);
             });
 
             $scope.$watch('useCustomDelimiter', function (data) {
-                parseData($scope.importRaw)
+                parseData($scope.importRaw, $scope.headerLength)
             });
 
             $scope.$watch('delimiter', function (data) {
-                parseData($scope.importRaw)
+                parseData($scope.importRaw, $scope.headerLength)
             });
 
 
