b17dce145e4158079c9af31441243c57fcd312f3
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-06-25 15:52:50 +0300

    Trying new approach

--- client/src/components/scoresheet/scoresheet.js
+++ client/src/components/scoresheet/scoresheet.js
@@ -5,10 +5,10 @@ export default {
 	<div class="top-bar-left">
 		<ul class="menu">
 			<li><ref-identity ng-if="scoresheet.isRef"></ref-identity></li>
-			<input type="text" ng-model="scoresheet.teamText()" editable-select="scoresheet.scoresheet.teamNumber" buttons="no" blur="submit"
+			<a type="text" ng-model="scoresheet.teamText()" editable-select="scoresheet.scoresheet.teamNumber" buttons="no" blur="submit"
 				class="menu-text" onaftersave="scoresheet.processErrors()"
 				e-ng-options="team.number as team.displayText for team in scoresheet.teams">
-		</input>
+		</a>
 		</ul>
 	</div>
 	<div class="top-bar-right flex-container">
