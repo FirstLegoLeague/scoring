9576f3b577e30e6418f97f71d00cbb3304f7b6b4
Jonathan Roberts Gideoni <yonatan.gideoni@gmail.com>
2018-07-02 20:53:16 +0300

    Added generic component team_search.

---
+++ client/src/components/generic/team_search.js
@@ -0,0 +1,15 @@
+'use strict'
+
+export default {
+    template: '<form>
+        <input type="text" list="teams" ng-model="scoresheet.scoresheet.teamNumber" ng-change="scoresheet.processErrors()">
+        <datalist id="teams">
+            <select>
+                <option type="text" ng-repeat="team in scoresheet.teams">
+                    {{ team.displayText }}
+                </option>
+            </select>
+        </datalist>
+</form>',
+    controller: 'GenericController as generic'
+}
