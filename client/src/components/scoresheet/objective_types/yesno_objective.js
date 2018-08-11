f13c9ce81fb0291972e4bf9bd6777db1d8c30d23
Idan Stark <idanstark42@gmail.com>
2018-08-11 17:49:40 +0300

    Some more fixes

--- client/src/components/scoresheet/objective_types/yesno_objective.js
+++ client/src/components/scoresheet/objective_types/yesno_objective.js
@@ -3,9 +3,9 @@
 export default {
 	template: `<div class="small button-group toggle" ng-click="objective.markAsComplete()">
     	<input type="radio" id="{{objective.data.id}}-no" name="{{objective.data.id}}" ng-model="objective.data.value" value="no">
-    	<label class="medium button" for="{{objective.data.id}}-no">No</label>
+    	<label class="button" ng-class="{'medium' : objective.data.value !== 'no' }" for="{{objective.data.id}}-no">No</label>
     	<input type="radio" id="{{objective.data.id}}-yes" name="{{objective.data.id}}" ng-model="objective.data.value" value="yes">
-    	<label class="medium button" for="{{objective.data.id}}-yes">Yes</label>
+    	<label class="button" ng-class="{'medium' : objective.data.value !== 'yes' }" for="{{objective.data.id}}-yes">Yes</label>
 	</div>`,
 	controller: 'ObjectiveController as objective',
 	bindings: {
