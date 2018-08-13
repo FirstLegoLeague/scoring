82658b23cf72d81a47c914fc967cfc4068d89074
Idan Stark <idanstark42@gmail.com>
2018-08-13 20:59:51 +0300

    Added missing files

---
+++ server/mhub_connection.js
@@ -0,0 +1,23 @@
+'use strict'
+/* eslint node/no-unsupported-features: 0 */
+
+const { MClient } = require('mhub')
+const logger = require('@first-lego-league/ms-logger').Logger()
+const { getCorrelationId } = require('@first-lego-league/ms-correlation')
+
+const MHUB_CLIENT_ID = 'cl-schedule'
+
+const mhubClient = new MClient(process.env.MHUB_URI)
+
+mhubClient.on('error', msg => {
+  logger.error('Unable to connect to mhub, other modules won\'t be notified changes \n ' + msg)
+})
+
+exports.publishMsg = function (topic, data = {}) {
+  return mhubClient.connect()
+    .then(() => mhubClient.login('protected-client', process.env.PROTECTED_MHUB_PASSWORD))
+    .then(() => mhubClient.publish('protected', topic, data, {
+      'client-id': MHUB_CLIENT_ID,
+      'correlation-id': getCorrelationId()
+    }))
+}
