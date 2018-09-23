db82c050fa8969be7432c00ed25fbcb8fba96cae
Tal Taub <taltaub22@gmail.com>
2018-09-23 18:12:38 +0300

    now checking if field is not undefined before trying to preform operations on it

--- client/src/controllers/scores/scores_controller.js
+++ client/src/controllers/scores/scores_controller.js
@@ -2,145 +2,151 @@
 
 class ScoresController {
 
-	constructor($scope, Configuration, Scores, Tournament, Messanger, Modals, User) {
-		this.$scope = $scope
-		this.Configuration = Configuration
-		this.Scores = Scores
-		this.Tournament = Tournament
-		this.Messanger = Messanger
-		this.Modals = Modals
-		this.search = ''
-		this.user = User.username
-		this.showDuplicates = false
-		this.showErrors = false
-		this.loading = true
-		this.match = null
-	}
+  constructor ($scope, Configuration, Scores, Tournament, Messanger, Modals, User) {
+    this.$scope = $scope
+    this.Configuration = Configuration
+    this.Scores = Scores
+    this.Tournament = Tournament
+    this.Messanger = Messanger
+    this.Modals = Modals
+    this.search = ''
+    this.user = User.username
+    this.showDuplicates = false
+    this.showErrors = false
+    this.loading = true
+    this.match = null
+  }
 
-	$onInit() {
-		// If the reload event comes from within this client, reload and send the message to every other client
-		// Otherwise just reload
-		this.$scope.$on('reload', () => this.load())
-		this.$scope.$on('alter', (event, callback) => {
-			this._scores = callback(this._scores)
-		})
-		this.$scope.$on('reload teams', () => this.loadTeams())
-		this.Messanger.on('scores:reload', () => this.load())
+  $onInit () {
+    // If the reload event comes from within this client, reload and send the message to every other client
+    // Otherwise just reload
+    this.$scope.$on('reload', () => this.load())
+    this.$scope.$on('alter', (event, callback) => {
+      this._scores = callback(this._scores)
+    })
+    this.$scope.$on('reload teams', () => this.loadTeams())
+    this.Messanger.on('scores:reload', () => this.load())
 
-		this.loadTeams()
-		this.Configuration.load().then(config => {
-			this.rankingsLink = config.rankings
-		})
-	}
+    this.loadTeams()
+    this.Configuration.load().then(config => {
+      this.rankingsLink = config.rankings
+    })
+  }
 
-	load() {
-		if(this.Scores.ignoreNextLoad) {
-			this.Scores.ignoreNextLoad = false
-		} else {
-			this.loading = true
-			this.$scope.$broadcast('reset')
-			this.Scores.load().then(scores => {
-				this._scores = scores
-				this.loading = false
-			})
-		}
-	}
+  load () {
+    if (this.Scores.ignoreNextLoad) {
+      this.Scores.ignoreNextLoad = false
+    } else {
+      this.loading = true
+      this.$scope.$broadcast('reset')
+      this.Scores.load().then(scores => {
+        this._scores = scores
+        this.loading = false
+      })
+    }
+  }
 
-	loadTeams() {
-		return this.Tournament.teams().then(teams => {
-			this._teamNumberList = []
-			for (var i = 0; i < teams.length; i++) {//Creates list of team numbers.
-				this._teamNumberList.push(teams[i].number)
-			}
-			return this.load()
-		})
-	}
+  loadTeams () {
+    return this.Tournament.teams().then(teams => {
+      this._teamNumberList = []
+      for (var i = 0; i < teams.length; i++) {//Creates list of team numbers.
+        this._teamNumberList.push(teams[i].number)
+      }
+      return this.load()
+    })
+  }
 
-	openDeletionDialog () {
-		this.Modals.open('#scores-deletion-modal')
-	}
+  openDeletionDialog () {
+    this.Modals.open('#scores-deletion-modal')
+  }
 
-	closeDeletionDialog () {
-		this.Modals.close('#scores-deletion-modal')
-	}
+  closeDeletionDialog () {
+    this.Modals.close('#scores-deletion-modal')
+  }
 
-	deleteAll() {
-		this.closeDeletionDialog()
-		this.deleting = true
-		this.Scores.deleteAll()
-			.then(() => {
-				this.$scope.$emit('alter', scores => [])
-				this.deleting = false
-			}).catch(() => {
-				this.Notifications.error('Unable to delete score: Possible network error.')
-				this.deleting = false
-			})
-	}
+  deleteAll () {
+    this.closeDeletionDialog()
+    this.deleting = true
+    this.Scores.deleteAll()
+      .then(() => {
+        this.$scope.$emit('alter', scores => [])
+        this.deleting = false
+      }).catch(() => {
+      this.Notifications.error('Unable to delete score: Possible network error.')
+      this.deleting = false
+    })
+  }
 
-	any() {
-		return this._scores ? this._scores.length > 0 : 0
-	}
+  any () {
+    return this._scores ? this._scores.length > 0 : 0
+  }
 
-	scores() {
-		let scores = this._scores
+  scores () {
+    let scores = this._scores
 
-		// Filter by search
-		if (this.search) {
-			scores = this._scores
-				.filter(score => [score.teamText.toLowerCase(), score.referee.toLowerCase(), score.tableText.toLowerCase(), score.matchText.toLowerCase(), score.score]
-					.some(value => (value || '').toString().includes(this.search.toLowerCase())))
-		}
+    // Filter by search
+    if (this.search) {
+      scores = this._scores
+        .filter(score => [score.teamText, score.referee, score.tableText, score.matchText, score.score]
+          .map(element => {
+            if (element && typeof element === 'string') {
+              return element.toLowerCase()
+            }
+            return element
+          })
+          .some(value => (value || '').toString().includes(this.search.toLowerCase())))
+    }
 
-		// Filter by showDuplicates
-		if (this.showDuplicates) {
-			scores = this.duplicateScores(scores)
+    // Filter by showDuplicates
+    if (this.showDuplicates) {
+      scores = this.duplicateScores(scores)
 
-			if (scores.length === 0) {
-				this.showDuplicates = false
-				scores = this._scores
-			}
-		}
+      if (scores.length === 0) {
+        this.showDuplicates = false
+        scores = this._scores
+      }
+    }
 
-		// Filter by showErrors
-		if (this.showErrors) {
-			scores = this.errorScores(scores)
+    // Filter by showErrors
+    if (this.showErrors) {
+      scores = this.errorScores(scores)
 
-			if (scores.length === 0) {
-				this.showErrors = false
-				scores = this._scores
-			}
-		}
+      if (scores.length === 0) {
+        this.showErrors = false
+        scores = this._scores
+      }
+    }
 
-		return scores
-	}
+    return scores
+  }
 
-	duplicateScores(scores) {
-		let self = this
-		scores = scores || this._scores || []
+  duplicateScores (scores) {
+    let self = this
+    scores = scores || this._scores || []
 
-		return scores.filter(score => {
-			return self._scores.some(otherScore => {
-				return score !== otherScore
-					&& otherScore.teamNumber === score.teamNumber && otherScore.matchId === score.matchId
-			})
-		})
-	}
+    return scores.filter(score => {
+      return self._scores.some(otherScore => {
+        return score !== otherScore
+          && otherScore.teamNumber === score.teamNumber && otherScore.matchId === score.matchId
+      })
+    })
+  }
 
-	errorScores(scores) {
-		scores = scores || this._scores || []
-		let duplicateErrors = this.duplicateScores(scores)
+  errorScores (scores) {
+    scores = scores || this._scores || []
+    let duplicateErrors = this.duplicateScores(scores)
 
-		let self = this
+    let self = this
 
-		let otherErrors = scores.filter(score =>
-			typeof score.teamNumber !== 'number' || typeof score.matchId === 'undefined' ||
-			(!this.loading && !this._teamNumberList.includes(score.teamNumber))
-		)
-		let badScores = duplicateErrors.concat(otherErrors)
-		badScores = badScores.filter(function (value, index) { return badScores.indexOf(value) == index })
+    let otherErrors = scores.filter(score =>
+      typeof score.teamNumber !== 'number' || typeof score.matchId === 'undefined' ||
+      (!this.loading && !this._teamNumberList.includes(score.teamNumber))
+    )
+    let badScores = duplicateErrors.concat(otherErrors)
+    badScores = badScores.filter(function (value, index) { return badScores.indexOf(value) == index })
 
-		return badScores;
-	}
+    return badScores
+  }
 }
 
 ScoresController.$$ngIsClass = true
