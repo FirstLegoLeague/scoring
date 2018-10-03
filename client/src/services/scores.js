7046e1095d8a74eef895c19b3d992a73e2b04855
Idan Stark <idanstark42@gmail.com>
2018-10-04 01:45:58 +0300

    Split the score into a seperate factory

--- client/src/services/scores.js
+++ client/src/services/scores.js
@@ -1,12 +1,6 @@
-const POSSIBLY_REQUIRED_FIELDS = {
-  requireRef: 'referee',
-  requireTable: 'tableId',
-  requireSignature: 'signature'
-}
-
 class Scores {
-  constructor ($http, tournament, messanger, configuration, independence, notifications) {
-    Object.assign(this, { $http, tournament, messanger, configuration, independence, notifications })
+  constructor ($http, score, tournament, messanger, configuration, independence, notifications) {
+    Object.assign(this, { $http, score, tournament, messanger, configuration, independence, notifications })
     this.scores = []
   }
 
@@ -28,7 +22,7 @@ class Scores {
   create (score) {
     this.messanger.ignoreNext('scores:reload')
     return this.configuration.load()
-      .then(config => this.independence.send('POST', '/scores/create', this._sanitizedScore(score, config)))
+      .then(config => this.independence.send('POST', '/scores/create', score.sanitize(config)))
       .then(scoreFromDB => { this.scores.push(this.score(scoreFromDB)) })
   }
 
@@ -52,85 +46,9 @@ class Scores {
       .then(() => { Object.assign(this.scores.find(score => score._id === id), attributes) })
       .catch(() => this.notifications.error('Unable to change score: Possible network error.'))
   }
-
-  _sanitizedScore (score, config) {
-    const sanitizedScore = {
-      missions: score.missions.map(mission => {
-        return {
-          id: mission.id,
-          score: mission.score,
-          objectives: mission.objectives.map(objective => {
-            return {
-              id: objective.id,
-              value: objective.value
-            }
-          })
-        }
-      }),
-      score: score.score,
-      challenge: score.title,
-      teamNumber: score.teamNumber,
-      round: score.round,
-      stage: score.stage,
-      matchId: score.matchId
-    }
-
-    Object.entries(POSSIBLY_REQUIRED_FIELDS).forEach(([configField, field]) => {
-      if (config[configField]) {
-        sanitizedScore[field] = score[field]
-      }
-    })
-
-    return sanitizedScore
-  }
-
-  score (attrs) {
-    const score = attrs
-
-    score.init = () => {
-      if (!score._initPromise) {
-        score._initPromise = score.load()
-      }
-      return score._initPromise
-    }
-
-    score.load = () => {
-      return Promise.all([this.tournament.loadTeamMatches(score.teamNumber), this.tournament.loadTeams(), this.tournament.loadTables()])
-        .then(([matches, teams, tables]) => {
-          score.matches = matches
-          score.match = matches.find(match => match.round === score.round && match.stage === score.stage)
-          score.team = teams.find(team => team.number === score.teamNumber)
-          score.table = matches.find(table => table.tableId === score.tableId)
-
-          score.matchError = Boolean(!score.match)
-          score.teamError = Boolean(!score.team)
-          score.noTable = Boolean(!score.table)
-
-          score.teamText = score.teamError ? 'Missing team' : score.team.displayText
-          score.matchText = score.matchError ? 'Missing round' : score.match.displayText
-          score.tableText = score.noTable ? 'No table' : score.table.tableName
-
-          score.ready = true
-        })
-    }
-
-    score.updateMatch = () => {
-      return this.tournament.loadTeamMatches(score.teamNumber)
-        .then(matches => {
-          score.matches = matches
-          score.matchId = matches.find(match => match.stage === score.stage && match.round === score.round)._id
-        })
-        .then(() => this.save())
-    }
-
-    score.teamText = score.matchText = score.tableText = 'Loading...'
-    score.teamError = score.matchError = score.ready = false
-
-    return score
-  }
 }
 
 Scores.$$ngIsClass = true
-Scores.$inject = ['$http', 'Tournament', 'Messanger', 'Configuration', 'Independence', 'Notifications']
+Scores.$inject = ['$http', 'Score', 'Tournament', 'Messanger', 'Configuration', 'Independence', 'Notifications']
 
 export default Scores
