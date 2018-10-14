6bffda2cdff8005121284758d592b2dc3c2715db
Idan Stark <idanstark42@gmail.com>
2018-10-14 16:20:22 +0300

    Only sending one exception each minute

--- client/src/factories/exception_handler.js
+++ client/src/factories/exception_handler.js
@@ -1,10 +1,26 @@
 function ExceptionHandler ($injector) {
+  const SAME_EXCEPTION_TIMEOUT = 60 * 1000 // One minute
+
   let logger
+  let lastExceptionMessage
+  let lastExceptionTime
+
+  function differentFromLastException (exception) {
+    const timeSinceLastException = Date.now() - lastExceptionTime
+    return lastExceptionMessage === undefined || exception.message !== lastExceptionMessage || timeSinceLastException > SAME_EXCEPTION_TIMEOUT
+  }
+
+  function setLastException (exception) {
+    lastExceptionMessage = exception.message
+    lastExceptionTime = Date.now()
+  }
 
   return function (exception, cause) {
-    logger = logger || $injector.get('Logger')
-    console.error(exception)
-    logger.error(`${exception.message} ${exception.stack}`)
+    if (differentFromLastException(exception)) {
+      logger = logger || $injector.get('Logger')
+      setLastException(exception)
+      logger.error(`${exception.message} ${exception.stack}`)
+    }
   }
 }
 
