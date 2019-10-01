39940af462cf5d488f9402b1e606527216cad4bb
johnmeshulam <johnmeshulam@gmail.com>
2019-10-01 18:06:14 +0300

    add admin privleges to development user

--- client/services/utils/user.js
+++ client/services/utils/user.js
@@ -1,6 +1,6 @@
 const USERNAME_COOKIE_KEY = 'username'
 const AUTH_TOKEN_COOKIE_KEY = 'auth-token'
-const ADMINS = ['admin', 'scorekeeper']
+const ADMINS = ['admin', 'scorekeeper', 'development']
 const REFS = ['referee', 'development']
 
 class User {
